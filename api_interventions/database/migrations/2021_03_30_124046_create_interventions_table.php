524dee88394ac7ca22827fe37ebc44257ddfff31
Kévin Oliva <olivakevin64@gmail.com>
2021-03-30 15:01:05 +0200

    Création de la table Interventions

---
+++ api_interventions/database/migrations/2021_03_30_124046_create_interventions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInterventionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interventions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('technicien_id');
+            $table->foreign('technicien_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+            $table->string('nomClient');
+            $table->string('prenomClient');
+            $table->string('adresseClient');
+            $table->string('marqueChaudiere');
+            $table->string('modeleChaudiere');
+            $table->date('dateMiseEnService');
+            $table->date('dateIntervention');
+            $table->string('numeroSerie');
+            $table->text('description');
+            $table->bigInteger('tempsPasse');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interventions');
+    }
+}
